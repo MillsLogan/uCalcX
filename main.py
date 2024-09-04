49779da5dd3c1ea12c0ef9a1b76030c76fd65644
Logan Mills <millslogan2003@gmail.com>
2024-09-04 10:25:53 -0400

    changes to main.py - in future make expand upon setup.py and make script more user friendly

--- main.py
+++ main.py
@@ -1,28 +1,9 @@
-import ucalcx as ucx
-from ucalcx import Measurement
-from ucalcx import Unit
-from ucalcx.mass import kilogram, gram
+from ucalcx.time_quantity import hour, year, minute, day
 
 
 def main():
-    import ucalcx as ucx
-    from ucalcx.length import Meter # Import the Meter class
-    kilometer = Meter(ucx.MetricPrefix.Kilo) # Adds the Kilo prefix to the Meter base unit
-    # ... imports as above
-    from ucalcx import Measurement
-    from ucalcx.length import meter
-    # Construction using mult operation definition
-    height_of_box = 5 * meter
-    print(height_of_box)
-    # Measurement(value=5, unit=FQUnit(meter, m, FundamentalQuantity.Length))
-    width_of_box = Measurement(10.2, meter)
-    print(width_of_box)
-    # Measurement(value=10.2, unit=FQUnit(kilometer, km, FundamentalQuantity.Length))
-    area_of_box = height_of_box * width_of_box
-    print(area_of_box)
-    from ucalcx.length.imperial import foot
-    area_in_sq_ft = area_of_box.convert_to(foot * foot)
-    print(area_in_sq_ft)
+    print((1 * year - 6 * hour) / (1 * year))
+    print((1 * day - 1 * minute) / (1 * day))
     
     
 if __name__ == "__main__":
